8356ff396abd44ed22ac97844d4e805d4dd4425c
Jorge Anais <jrganais@gmail.com>
2022-12-30 12:19:27 -0300

    Change order

--- euler_angles.py
+++ euler_angles.py
@@ -15,17 +15,17 @@ def get_matrix(theta: float, phi: float, psi: float):
     # Rotation about z-axis
     D = np.array(
         [
-            [cosθ, sinϕ, 0],
-            [-sinθ, cosϕ, 0],
-            [0, 0, 1]
+            [ cosθ,  sinϕ, 0],
+            [-sinθ,  cosϕ, 0],
+            [    0,     0, 1]
         ]
     )
 
     # Rotation about ξ-axis
     C = np.array(
         [
-            [1, 0, 0],
-            [0, cosθ, sinθ],
+            [1,    0,     0],
+            [0,  cosθ, sinθ],
             [0, -sinθ, cosθ]
         ]
     )
@@ -33,9 +33,9 @@ def get_matrix(theta: float, phi: float, psi: float):
     # Rotation about ζ'-axis
     B = np.array(
         [
-            [cosψ, sinψ, 0],
+            [ cosψ, sinψ, 0],
             [-sinψ, cosψ, 0],
-            [0, 0, 1]
+            [    0,    0, 1]
         ]
     )
 
